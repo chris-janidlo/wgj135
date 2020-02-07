49f6dbe17ea2493d1adaf0ede526e27639161a2d
crassSandwich <christopherjanidlo@gmail.com>
2020-02-07 18:39:38 -0500

    Colors n pixelation

---
+++ Assets/Scripts/Colors.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using crass;
+
+public class Colors : Singleton<Colors>
+{
+    public Color On, Off;
+
+    void Awake ()
+    {
+        SingletonSetInstance(this, true);
+    }
+}
