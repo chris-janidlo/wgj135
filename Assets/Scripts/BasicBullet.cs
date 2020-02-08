0f41853548a07653b767b906119ee8f35646ffda
crassSandwich <christopherjanidlo@gmail.com>
2020-02-08 11:29:47 -0500

    Shooty shooty

---
+++ Assets/Scripts/BasicBullet.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicBullet : MonoBehaviour
+{
+    public float Damage;
+    public Rigidbody Rigidbody;
+
+    void OnCollisionEnter (Collision other)
+    {
+        // do damage
+    }
+}
---
+++ Assets/Scripts/BasicGun.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicGun : MonoBehaviour
+{
+    public BasicBullet BulletPrefab;
+
+    public float FiringForceMagnitude, HeatPerShot, TimePerShot;
+    public string FireButton, SwitchDirectionButton;
+
+    public Rigidbody PlayerRigidbody;
+
+    void Start ()
+    {
+        StartCoroutine(shootine());
+    }
+
+    void Update ()
+    {
+        bool shouldSwitch = Input.GetButton(SwitchDirectionButton);
+
+        transform.localEulerAngles = new Vector3(0, shouldSwitch ? 180 : 0, 0);
+    }
+
+    IEnumerator shootine ()
+    {
+        while (true)
+        {
+            if (Input.GetButton(FireButton))
+            {
+                Vector3 firingForce = transform.forward * FiringForceMagnitude;
+
+                BasicBullet bullet = Instantiate(BulletPrefab, transform.position, Quaternion.Euler(firingForce));
+
+                bullet.Rigidbody.AddForce(firingForce, ForceMode.VelocityChange);
+                PlayerRigidbody.AddForce(-firingForce, ForceMode.VelocityChange);
+
+                // subtract heat
+
+                yield return new WaitForSeconds(TimePerShot);
+            }
+            else
+            {
+                yield return null;
+            }
+        }
+    }
+}
