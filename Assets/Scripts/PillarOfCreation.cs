8e288a2a600d70570cbd6155625d165babeed55a
crassSandwich <christopherjanidlo@gmail.com>
2020-02-12 20:54:45 -0500

    Spawn algo improvements

--- Assets/Scripts/PillarOfCreation.cs
+++ Assets/Scripts/PillarOfCreation.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -19,47 +20,80 @@ public class PillarOfCreation : MonoBehaviour
 
     void Start ()
     {
-        ResourceBag potentialGoals = new ResourceBag(0, 0, 0, 0);
-
-        for (int i = 0; i < GuaranteedBodyPrefabs.Items.Count; i++)
+        List<AstronomicalBody> prefabs = getPrefabsToSpawn();
+        List<Vector3> unitSphereDistribution = fibonacciSphere(prefabs.Count, true);
+        
+        for (int i = 0; i < prefabs.Count; i++)
         {
-            var spawned = spawnAstronomicalBody(GuaranteedBodyPrefabs);
+            Vector3 point;
 
-            potentialGoals += spawned.ResourceProfile;
+            do
+            {
+                point = transform.position + unitSphereDistribution[i] * RandomExtra.Range(DistanceFromSpawnRange);
+            } while (Physics.CheckSphere(point, prefabs[i].MinDistanceFromOtherColliders));
+
+            Instantiate(prefabs[i], point, UnityEngine.Random.rotation);
         }
+
+        Goals = getGoals(prefabs);
+    }
+
+    List<AstronomicalBody> getPrefabsToSpawn ()
+    {
+        List<AstronomicalBody> prefabs = new List<AstronomicalBody>(GuaranteedBodyPrefabs.Items);
 
         for (int i = 0; i < RandomExtra.Range(ExtraBodiesToSpawnRange); i++)
         {
-            var spawned = spawnAstronomicalBody(ExtraBodyPrefabs);
-
-            potentialGoals += spawned.ResourceProfile;
+            prefabs.Add(ExtraBodyPrefabs.GetNext());
         }
 
-        Goals = new ResourceBag
+        prefabs.ShuffleInPlace();
+
+        return prefabs;
+    }
+
+    // translated from python version at https://stackoverflow.com/a/26127012/5931898
+    List<Vector3> fibonacciSphere (int samples, bool randomize)
+    {
+        float rand = randomize ? UnityEngine.Random.Range(0f, samples) : 1;
+
+        List<Vector3> points = new List<Vector3>();
+        float offset = 2f / samples;
+        float increment = Mathf.PI * (3 - Mathf.Sqrt(5f));
+
+        for (int i = 0; i < samples; i++)
+        {
+            float y = i * offset - 1 + offset / 2;
+            float r = Mathf.Sqrt(1 - Mathf.Pow(y, 2));
+            float phi = ((i + rand) % samples) * increment;
+
+            points.Add(new Vector3
+            (
+                Mathf.Cos(phi) * r,
+                y,
+                Mathf.Sin(phi) * r
+            ));
+        }
+
+        return points;
+    }
+
+    ResourceBag getGoals (List<AstronomicalBody> bodies)
+    {
+        ResourceBag potentialGoals = new ResourceBag(0, 0, 0, 0);
+
+        foreach (ResourceBag bag in bodies.Select(b => b.ResourceProfile))
+        {
+            potentialGoals += bag;
+        }
+
+        return new ResourceBag
         (
             0,
-            UnityEngine.Random.Range(potentialGoals.Hydrogen / 2, potentialGoals.Hydrogen),
-            UnityEngine.Random.Range(potentialGoals.Methane / 2, potentialGoals.Methane),
-            UnityEngine.Random.Range(potentialGoals.Silicon / 2, potentialGoals.Silicon)
+            UnityEngine.Random.Range(potentialGoals.Hydrogen / 2, potentialGoals.Hydrogen) / 10 * 10,
+            UnityEngine.Random.Range(potentialGoals.Methane / 2, potentialGoals.Methane) / 10 * 10,
+            UnityEngine.Random.Range(potentialGoals.Silicon / 2, potentialGoals.Silicon) / 10 * 10
         );
-    }
-
-    AstronomicalBody spawnAstronomicalBody (AstronomicalBodyBag bag)
-    {
-        AstronomicalBody next = bag.GetNext();
-        return Instantiate(next, findUnoccupiedPosition(next.MinDistanceFromOtherColliders), UnityEngine.Random.rotation, AstronomicalBodyParent);
-    }
-
-    public Vector3 findUnoccupiedPosition (float minDistance)
-    {
-        Vector3 position;
-        
-        do
-        {
-            position = transform.position + UnityEngine.Random.onUnitSphere * RandomExtra.Range(DistanceFromSpawnRange);
-        } while (Physics.CheckSphere(position, minDistance));
-
-        return position;
     }
 }
 
