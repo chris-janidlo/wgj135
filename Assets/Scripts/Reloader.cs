02942e5513fa3feb2735629011de395062dee62a
crassSandwich <christopherjanidlo@gmail.com>
2020-02-13 20:17:44 -0500

    Polish up those menus
    cause I am one hungry customer

---
+++ Assets/Scripts/Reloader.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Reloader : MonoBehaviour
+{
+    public float Delay;
+    public string TargetScene;
+
+    IEnumerator Start ()
+    {
+        yield return new WaitForSeconds(Delay);
+
+        while (true)
+        {
+            if (Input.anyKey)
+                SceneManager.LoadScene(TargetScene);
+            
+            yield return null;
+        }
+    }
+}
